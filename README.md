d36109086fe042ed64be817ff59d32a4a6d5ff4b
StefanoGalandrini <stefe68@gmail.com>
2023-04-26 11:27:27 +0200

    Init repo with last exercise files to create grid

---
+++ README.md
@@ -0,0 +1,9 @@
+# js-campominato-dom
+
+# There's always a problem when, with some widths and number of cells in a row, you can see small empty spaces between the cells and the border of the container.
+
+# As in the previous exercise, the base dimension of the container is defined in javascript and can be modified.
+
+# Also, the width and height of cells is rounded to an integer. Then is multiplied for the number of cells in a row and applied as an inline style to the container.
+
+#
---
+++ assets/script.js
@@ -0,0 +1,68 @@
+// declare global DOM variables
+const playBtn = document.querySelector("#play");
+const grid = document.querySelector(".grid");
+
+// defines the approx centainer width and height in pixels
+const baseContainerDim = 600;
+
+// add event listener to play button
+// all code will be inside that
+playBtn.addEventListener("click", function () {
+	grid.innerHTML = "";
+
+	const difficultyLevel = document.querySelector("#difficulty").value;
+
+	// build grid creating html elements
+	gridDimension(difficultyLevel);
+	let cellCount = totalCells;
+	let cellRow = Math.sqrt(totalCells);
+	const dimCell = cellDimension(cellRow);
+
+	for (let i = 1; i <= cellCount; i++) {
+		const cell = document.createElement("div");
+		cell.classList.add("cell");
+		cell.style.width = `${dimCell}px`;
+		cell.style.height = `${dimCell}px`;
+		cell.innerHTML = i;
+
+		cell.addEventListener("click", function () {
+			cell.classList.toggle("selected");
+			console.log(`Hai cliccato sulla casella ${i}`);
+		});
+
+		grid.appendChild(cell);
+	}
+	/*	dimensioning container to an integer
+		plus twice the border
+		to prevent small empty spaces between cells and container
+		with some widths and number of cells */
+	dimContainer = dimCell * cellRow + 6;
+	grid.style.opacity = 1;
+	grid.style.maxWidth = `${dimContainer}px`;
+	grid.style.maxHeight = `${dimContainer}px`;
+});
+
+function gridDimension(level) {
+	// define grid dimension based on selection
+	switch (level) {
+		case "1":
+			totalCells = 100;
+			break;
+		case "2":
+			totalCells = 81;
+			break;
+		case "3":
+			totalCells = 49;
+			break;
+		default:
+			totalCells = 100;
+	}
+	return totalCells;
+}
+
+function cellDimension(dim) {
+	//value rounded to integer to prevent
+	// small empty spaces between cells and container
+	const dimCell = Math.round((baseContainerDim - 2 * 3) / dim);
+	return dimCell;
+}

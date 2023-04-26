4bf59c81d3b873ad60f6b102221bbb23d0315a2b
StefanoGalandrini <stefe68@gmail.com>
2023-04-26 15:21:35 +0200

    Add working function to detect mines & keep score

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,22 @@
 // declare global DOM variables
 const playBtn = document.querySelector("#play");
 const grid = document.querySelector(".grid");
+const start = document.querySelector(".start");
+let endGame = false;
 
 // defines the approx centainer width and height in pixels
 const baseContainerDim = 600;
 
 // Variable for score keeping and mines numbers
-let totalPoints = 0;
 let totalCells = 0;
 
 // add event listener to play button
 // all code will be inside that
 playBtn.addEventListener("click", function () {
+	start.innerHTML = "";
 	grid.innerHTML = "";
 	const minesArray = [];
+	let totalPoints = 0;
 
 	const difficultyLevel = document.querySelector("#difficulty").value;
 
@@ -25,6 +28,7 @@ playBtn.addEventListener("click", function () {
 
 	// generate Array with 16 random numbers for mines
 	createMinesArray(1, totalCells, 16, minesArray);
+	console.log(minesArray);
 
 	const cellArray = [];
 	for (let i = 0; i < cellCount; i++) {
@@ -32,25 +36,18 @@ playBtn.addEventListener("click", function () {
 		cellArray[i].classList.add("cell");
 		cellArray[i].style.width = `${dimCell}px`;
 		cellArray[i].style.height = `${dimCell}px`;
-		cellArray[i].innerHTML = i;
+		cellArray[i].innerHTML = i + 1;
 
 		cellArray[i].addEventListener("click", function () {
-			console.log(`Hai cliccato sulla casella ${i}`);
-
-			if (minesArray.includes(i)) {
-				for (let i = 0; i < minesArray.length; i++) {
-					cellArray[minesArray[i]].classList.add("mine");
-				}
-			} else {
-				cellArray[i].classList.toggle("selected");
-				totalPoints++;
-			}
+			console.log(`Hai cliccato sulla casella ${i + 1}`);
+			checkBomb(i, minesArray, cellArray);
+			totalPoints++;
+			console.log(endGame, totalPoints);
 		});
 
 		grid.appendChild(cellArray[i]);
 	}
-	/*	dimensioning container to an integer
-		plus twice the border
+	/*	dimensioning container to an integer plus twice the border
 		to prevent small empty spaces between cells and container
 		with some widths and number of cells */
 	dimContainer = dimCell * cellRow + 6;
@@ -97,4 +94,16 @@ function createMinesArray(min, max, mines, minesArr) {
 	}
 	const sortedArray = minesArr.sort((a, b) => a - b);
 	return sortedArray;
+}
+
+function checkBomb(index, minesArray, cellArray) {
+	if (minesArray.includes(index + 1)) {
+		for (let i = 0; i < minesArray.length; i++) {
+			cellArray[minesArray[i] - 1].classList.add("mine");
+		}
+		endGame = true;
+	} else {
+		cellArray[index].classList.toggle("selected");
+		endGame = false;
+	}
 }
